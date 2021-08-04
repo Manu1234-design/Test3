737d14abdc6735216032f69db44f9c80780d03a0
DESKTOP-PL3AINM\GOVT ITI WOMENS <peringhatmanu@gmail.com>
2021-08-04 19:58:31 +0530

    PlayerScript

--- My project/Assets/PlayerMotor.cs
+++ My project/Assets/PlayerMotor.cs
@@ -1,18 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
 {
+
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Rigidbody rb;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        PerformMovement();
+        PerformRotation();
     }
+
+    public void Rotate (Vector3 _rotation){
+        rotation = _rotation;
+    }
+
+    public void RotateCamera (Vector3 _cameraRotation){
+        cameraRotation = _cameraRotation;
+    }
+
+    public void Move (Vector3 _velocity){
+        velocity = _velocity;
+    }
+
+    
+
+
+    void PerformMovement (){
+        if (velocity != Vector3.zero){
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    void PerformRotation(){
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null){
+            cam.transform.Rotate(-cameraRotation);
+        }
+    }
+
+
+
+
 }
